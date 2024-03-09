ba49bb2fdba8c393154afa9e71c73577fdc0d160
sakamoto-kohei-44 <banbengongping111@gmail.com>
2024-03-09 22:38:58 +0900

    一時的なコミット

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\post;
 
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('post.index');
+    }
 }
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+    // コメントを作成したユーザー
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // このコメントが属している投稿
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
